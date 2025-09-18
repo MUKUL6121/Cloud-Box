990ba2cb7370f93a0055c81ac8f7db73cabfaa4a
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-09-18 19:12:10 +0530

    Update Nav.jsx

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,8 +6,8 @@ export default function form() {
             <br />
             <div>
                 <ul style={{ listStyle: "none", padding: "0", display: "flex", flexDirection: "row", gap: "10px" }}>
-                    <li><Link to="http://localhost:5173/files">View Uploaded Files</Link></li><br />
-                    <li><Link to="http://localhost:5173/Form">Upload Another File</Link></li><br />
+                    <li><Link to="/files">View Uploaded Files</Link></li><br />
+                    <li><Link to="/Form">Upload Another File</Link></li><br />
                     {/* <li><Link to="http://localhost:5500/">Go to Home</Link></li><br /> */}
                 </ul>
             </div>
