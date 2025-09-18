f027404af60ada9f4d9abc5965dc8995b2f00f78
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-09-18 19:17:05 +0530

    Update Form.jsx

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,7 +1,7 @@
 function Form() {
     return (
         <div>
-            <form id="upload Form" action="http://localhost:5500/upload" className="border border-1 border-black p-3" method="post" encType="multipart/form-data">
+            <form id="upload Form" action="https://cloud-box-server.onrender.com/upload" className="border border-1 border-black p-3" method="post" encType="multipart/form-data">
                 <input type="file" name="samplefile" required multiple />
                 <button type="submit">Upload</button>
             </form>
