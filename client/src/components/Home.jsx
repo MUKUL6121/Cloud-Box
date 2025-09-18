e6a296492b587b772424d2717c62eeeb6df0fe83
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-09-18 19:15:50 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,8 +2,8 @@ function Home() {
     return (
         <div>
             <ul style={{ listStyle: "none", padding: "0", display: "flex", flexDirection: "row", gap: "10px" }}>
-                <li><a href="http://localhost:5500/files">View Uploaded Files</a></li><br />
-                <li><a href="http://localhost:5173/Form">Upload Another File</a></li><br />
+                <li><a href="/files">View Uploaded Files</a></li><br />
+                <li><a href="/Form">Upload Another File</a></li><br />
                 {/* <li><a href="http://localhost:5500/">Go to Home</a></li><br /> */}
             </ul>
         </div>
